2f0f4321b14032aad13d188cb9871830f2155983
Tariro Kandemiri <kandeta0@sewanee.edu>
2017-01-30 20:56:46 -0600

    Update food.blade.php
    Not sure if we need to do food.li or food.lu, can you make it work w/ your css?

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -2,10 +2,9 @@
 <head>
     <link rel="icon" href="{{ asset('images/mtneats.png') }}">
     <title> Mountain Eats </title>
-    <link rel="stylesheet" type="text/css" href="eats.css">
+    <link rel="stylesheet" type="text/css" href="home.css">
 
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,900,900i" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/food.css') }}">
 </head>
 <body>
 
@@ -15,8 +14,23 @@
     <li><a href="#about">ABOUT</a></li>
 </ul>
 
+<p>
+
+Click the name of the restaurant you would like to order from: 
 <br>
 <br>
 
+<food.ul>
+  <food.li><a href="http://www.w3schools.com/html/">Papa Rons</a></food.li>
+  <br>
+  <food.li><a href="http://www.w3schools.com/html/">Wendys</a></food.li>
+  <br>
+  <food.li><a href="http://www.w3schools.com/html/">Sonic</a></food.li>
+  <br>
+  <food.li><a href="http://www.w3schools.com/html/">Stirlings</a></food.li>
+  <br>
+  <food.li><a href="http://www.w3schools.com/html/">Mountain Goats</a></food.li>
+  </food.ul>
+</p>
+
 </body>
-</html>
