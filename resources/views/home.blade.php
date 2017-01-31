9491da645e8e6560415006b2872f00228d1ae975
Daniel <evansdb0@sewanee.edu>
2017-01-30 20:38:43 -0600

    Updated structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!-- Tariro Kandemiri - 2017 -->
+
+<html>
+<head>
+    <link rel="icon" href="mtneats.png">
+    <title> Mountain Eats </title>
+    <link rel="stylesheet" type="text/css" href=" {{ asset('css/home.css') }}">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,900,900i" rel="stylesheet">
+</head>
+<body>
+
+<ul>
+    <li><a class="active" href="#home">MOUNTAIN EATS</a></li>
+    <li><a href="http://tarirokandemiri.com/MountainEats/food.html">ORDER ONLINE</a></li>
+    <li><a href="#about">ABOUT</a></li>
+
+    <img src="mtneats.png"  style="width:150px !important;height:75px !important; padding: 15px 50px; align: right;" />
+</ul>
+<figure>
+    <img src="http://i67.tinypic.com/2w67w39.png" alt="Smiley face" align="middle">
+    <a href="http://tarirokandemiri.com/MountainEats/food.html" class="btn">ORDER NOW</a>
+</figure>
+
+</body>
+</html>
