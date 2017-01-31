31129b1cb7eb84ca1b3e73a818c63a6bb90b7ef2
Daniel <evansdb0@sewanee.edu>
2017-01-30 20:20:56 -0600

    Added laravel configuration

---
+++ resources/views/global_config.blade.php
@@ -0,0 +1,21 @@
+{{-- JQuery --}}
+<script src= {{ asset('js/app.js') }}></script>
+{{-- Bootstrap --}}
+<link rel="stylesheet" href= {{ asset('css/app.css') }}>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<script>
+  // debugging helper function
+  function p($obj) {
+    console.log($obj);
+  }
+</script>
+
+
+{{--Angular JS Scripts --}}
+<!--<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>-->
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular-resource.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
