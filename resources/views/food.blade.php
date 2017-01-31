c066ff8b2d93ada35799b69a3fb0ba7a1121b138
Daniel <evansdb0@sewanee.edu>
2017-01-30 20:49:35 -0600

    Updated structure

---
+++ resources/views/food.blade.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <link rel="icon" href="{{ asset('images/mtneats.png') }}">
+    <title> Mountain Eats </title>
+    <link rel="stylesheet" type="text/css" href="eats.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/food.css') }}">
+</head>
+<body>
+
+<ul>
+    <li><a class="active" href="home.html">MOUNTAIN EATS</a></li>
+    <li><a href="#contact">ORDER ONLINE</a></li>
+    <li><a href="#about">ABOUT</a></li>
+</ul>
+
+<br>
+<br>
+
+</body>
+</html>
