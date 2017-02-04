65db5327c5d5038f28fd9faea75179eb27f60a8b
tkandemiri <kandeta0@sewanee.edu>
2017-02-03 21:00:03 -0600

    Updated CSS for checkout page, also rearranged page.

--- resources/views/list_restaurants.blade.php
+++ resources/views/list_restaurants.blade.php
@@ -1,35 +1,39 @@
 @extends('layout')
 
 @section('head')
+    <link rel="stylesheet" href="{{ asset("css/list.css") }}">
     <title>Sewanee Eats | Restaurants</title>
 @stop
 
 @section('body')
-    <div class="container" id="list-restaurants">
+    <div>
 
 
         <!--
              Can you guys figure out how to lower the body so that everything shows
              up without using <br>??
         -->
-        <br><br><br><br>
-        <section id="places" class="container">
-            <ul class="list-group">
+
+        <div class="divider"></div>
+        <section id="places">
+            <h1>Choose A Restaurant Below:</h1>
+
+            <ul.rest>
                 @if(count($restaurants) == 0)
                     <h1>All restaurants are closed at this time</h1>
                 @else
                     @foreach($restaurants as $restaurant)
-                    <li class="list-group-item">
+                    <li>
                         <a href="{{ route('showMenu',['id' => $restaurant->id]) }}">
-                            <div class="container">
-                                <h1 class="list-group-item-heading">{{ $restaurant->name }}</h1>
-                                <p class="list-group-item-info">{{ $restaurant->description }}</p>
+                            <div>
+                                <h2 class="list-group-item-heading">{{ $restaurant->name }}</h2>
+                                <p>{{ $restaurant->description }}</p>
                             </div>
                         </a>
                     </li>
                     @endforeach
                 @endif
-            </ul>
+            </ul.rest>
         </section>
     </div>
 @stop
