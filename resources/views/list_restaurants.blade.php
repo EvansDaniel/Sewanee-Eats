7a228c79fe4f36a5b8bb6b31179701e49684eb9b
Daniel Bryce Evans <evansdb0@sewanee.edu>
2017-02-04 12:23:38 -0600

    changed list_restaurants to blaise's version

--- resources/views/list_restaurants.blade.php
+++ resources/views/list_restaurants.blade.php
@@ -7,27 +7,6 @@
 
 @section('body')
 
-    <div class="container" id="list-restaurants">
-        <section id="places" class="container">
-            <ul class="list-group">
-                @if(count($restaurants) == 0)
-                    <h1>All restaurants are closed at this time</h1>
-                @else
-                    @foreach($restaurants as $restaurant)
-                        <li class="list-group-item">
-                            <a href="{{ route('showMenu',['id' => $restaurant->id]) }}">
-                                <div class="container">
-                                    <img style="width: 200px" src="{{ $restaurant->image_url }}" alt="Restuarant Image">
-                                    <h1 class="list-group-item-heading">{{ $restaurant->name }}</h1>
-                                    <p class="list-group-item-info">{{ $restaurant->description }}</p>
-                                </div>
-                            </a>
-                        </li>
-                    @endforeach
-                @endif
-            </ul>
-        </section>
-
     {{--<div>--}}
 
 
