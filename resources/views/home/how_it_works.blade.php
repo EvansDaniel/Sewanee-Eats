df6bb75747c464bd0ab23a4431bcbe74a3fcc993
tkandemiri <kandeta0@sewanee.edu>
2017-03-02 21:28:51 -0600

    Put information regarding how Sewanee Eats service works, as well as contact information.

--- resources/views/home/how_it_works.blade.php
+++ resources/views/home/how_it_works.blade.php
@@ -2,7 +2,50 @@
 
 
 @section('body')
+    <center>
 
-    <h1>How it works</h1>
+    <h2>How do I place an order?</h2>
+
+    <p style="width:50%; padding: 0 15 0 50"> Ordering from your favorite place with Sewanee Eats is a breeze! Just pick a restaurant then add your desired
+    food items to the cart.
+    </p>
+
+    <h2>How do I pay for my food?</h2>
+
+
+    <p style="width:50%; padding: 0 15 0 50">Click the Checkout link in the top right corner and you will be directed to
+    a page where you can pay for your order. You can pay the following ways:
+    <p>
+
+    <h3>Paying With Venmo</h3>
+
+
+    <p style="width:50%; padding: 0 15 0 50">To pay with Venmo, leave your Venmo username in the empty box and click 'Submit'. We will immediately send you a request on Venmo
+        for the amount listed as your order total. Once you have paid, you are good to go!</p>
+
+    <h3>Pay with Card</h3>
+
+    <p style="width:50%; padding: 0 15 0 50">
+    We accept all major debit and credit cards. Simply enter your information and click 'Submit'. Our payment processor is
+    secure and all your information is safe. We process all card payments using <a href="https://www.stripe.com">Stripe</a>, a secure payment processor.
+    </p>
+
+    <h2>I've paid, now what?</h2>
+    <p style="width:50%; padding: 0 15 0 50">
+    Once you have paid for your order, you will receive an email receipt from Sewanee Eats confirming your order. One of our delivery
+    carriers will go and pick up your order and deliver it to you within 30-45 minutes.
+    <p>
+
+    <h2>Help! There was a problem with my order!</h2>
+    <p style="width:50%; padding: 0 15 0 50">
+    We will always do our best to resolve your order problems. If there is an issue with your order, please email us immediately <a href="mailto:sewaneeeats@gmail.com?Subject=Order%20Issue" target="_top">here</a> and
+    we will fix the issue.
+        <br>
+    Thank you for supporting Sewanee Eats!
+    </p>
+
+    </center>
+
 
 @stop
+
