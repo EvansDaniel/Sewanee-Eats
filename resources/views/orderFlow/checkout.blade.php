e598e4f8e63e66cb5bbb83a395f58bb424fa3d5f
blezzoh <iradub0@sewanee.edu>
2017-03-04 16:28:16 -0600

    new looks on checkout

--- resources/views/orderFlow/checkout.blade.php
+++ resources/views/orderFlow/checkout.blade.php
@@ -32,10 +32,15 @@
                 <h1 id="cart-title" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     Thank you for shopping with us. This is your order summary,
                 </h1>
+
+            <div class="row">
                 <h3 class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cart-review">
                     1.Review item(s)
                 </h3>
-                <hr>
+
+
+            </div>
+
 
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="orders">
 
@@ -70,14 +75,17 @@
                 <div class="cart " id="main-payment-form" style="">
                     <!-- Payment information -->
 
-                    <h3 class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cart-review">
-                        2. Enter your information to pay:
-                    </h3>
-                    <hr>
+
+                    <div class="row">
+                        <h3 class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cart-review">
+                            2. Enter your information to pay:
+                        </h3>
+                    </div>
+
                     <div id="ct-ct">
                         <div class="form-group">
                             <div class="row">
-                                <div class="col-lg-3 col-md-3 col-sm-10 col-xs-10">
+                                <div class="col-lg-4 col-md-4 col-sm-10 col-xs-10">
                                     <label for="pay-with-venmo" id="pay-with-what">Pay with Venmo?</label>
                                     <input type="checkbox" name="pay_with_venmo" id="pay-with-venmo" value="0">
                                     <div id="venmo-payment-div" class="form-group">
@@ -88,11 +96,14 @@
 
                                     </div>
                                 </div>
+                            </div>
+                            <div class="row">
+
 
                                 <div id="card-payment-div">
 
                                     <span class="" style="display: none;" id="payment-errors"></span>
-                                    <div class="form-group  col-lg-3 col-md-3 col-sm-10   col-xs-10" id="c-date">">
+                                    <div class="form-group  col-lg-3 col-md-3 col-sm-10   col-xs-10" id="c-number">
                                         <label>
                                             <span class="row">Card Number</span>
                                             <input class="pay-input form-control" type="text" id="card-number" size="20"
@@ -100,7 +111,7 @@
                                         </label>
                                     </div>
 
-                                    <div class="form-group" id="c-date">
+                                    <div class="form-group col-lg-3 col-md-3 col-sm-10  col-xs-10" id="c-date">
                                         <label>
                                             <span class="row">Expiration (MM/YY)</span>
                                             <input class="pay-input" type="text" size="2" id="exp-month" maxlength="2"
@@ -119,49 +130,39 @@
                                         </label>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="form-group" id="loc-phone">
-                            @if(!empty($items['non_special_items']))
-                                <label for="location">Where should we deliver your On Demand items?</label>
-                                <input class="form-control pay-input" type="text" maxlength="100" name="location"
-                                       id="location">
-                            @endif
-                            <label for="email-address">Email Address</label>
-                            <input class="form-control pay-input" maxlength="100"
-                                   placeholder="Please enter your email address"
-                                   type="email"
-                                   name="email_address" id="email-address">
-                            {{--
-                           <input class="form-control pay-input" maxlength="10" placeholder="10 digits, only numbers"
-                                  type="tel"
-                                  name="phone_number" id="phone-number">--}}
-                        </div>
-                        <div>Subtotal: $<span id="subtotal">{{ $subtotal }}</span></div>
-                        <div>Order Total: $<span id="total-price">{{ $total_price }}</span></div>
-                        <button type="submit" id="pay-now-button" onclick="checkPayNow(event)"
-                                class="checkout-btn">Pay Now
-                        </button>
-                    </div>
-                </div>
-        @endif
 
-        <!-- Show checkout info only if we are on the checkout page -->
+                            </div>
+                            <div class="form-group" id="loc-phone">
+                                @if(!empty($items['non_special_items']))
+                                    <label for="location">Where should we deliver your On Demand items?</label>
+                                    <input class="form-control pay-input" type="text" maxlength="100" name="location"
+                                           id="location">
+                                @endif
+                                <label for="email-address">Email Address</label>
+                                <input class="form-control pay-input" maxlength="100"
+                                       placeholder="Please enter your email address"
+                                       type="email"
+                                       name="email_address" id="email-address">
+                                {{--
+                               <input class="form-control pay-input" maxlength="10" placeholder="10 digits, only numbers"
+                                      type="tel"
+                                      name="phone_number" id="phone-number">--}}
+                            </div>
+                            <div>Subtotal: $<span id="subtotal">{{ $subtotal }}</span></div>
+                            <div>Order Total: $<span id="total-price">{{ $total_price }}</span></div>
+                            <button type="submit" id="pay-now-button" onclick="checkPayNow(event)"
+                                    class="checkout-btn">Pay Now
+                            </button>
+                        </div>
+                    </div>
+                @endif
+
+                <!-- Show checkout info only if we are on the checkout page -->
         </form>
     </div>
 
     <style>
-        #pay-now-button {
-            background: linear-gradient(90deg, #240a54 10%, #7459a5 90%);
-            color: white;
-            padding: 1% 4%;
-            border-radius: 10px;
-            letter-spacing: .12em;
-            font-size: 16px;
-            font-family: 'Lato', sans-serif;
-            font-weight: bold;
-            border: solid rebeccapurple 2px;
-        }
+
 
         .hr-separator {
             display: block;
@@ -178,77 +179,77 @@
 
     <!-- Strip payment script -->
     <script>
-      Stripe.setPublishableKey('pk_test_GALLn3YWDPqPycDBzdxuMz2z');
+        Stripe.setPublishableKey('pk_test_GALLn3YWDPqPycDBzdxuMz2z');
 
-      $(function () {
-        var $form = $('#payment-form');
-        $form.submit(function (event) {
-          $('#pay-now-button').prop('disabled', true);
-          if (!$('#pay-with-venmo').is(':checked')) {
-            // Disable the submit button to prevent repeated clicks:
+        $(function () {
+            var $form = $('#payment-form');
+            $form.submit(function (event) {
+                $('#pay-now-button').prop('disabled', true);
+                if (!$('#pay-with-venmo').is(':checked')) {
+                    // Disable the submit button to prevent repeated clicks:
 
-            // TODO: see where this fits into the current set up
-              /*var message = validPayForm(true);
-               if (message !== null) { // an error message was returned
-               $('#payment-errors').show().text(message);
-               event.preventDefault();
-               $form.find('.submit').prop('disabled', false);
-               return false;
-               }*/
+                    // TODO: see where this fits into the current set up
+                    /*var message = validPayForm(true);
+                     if (message !== null) { // an error message was returned
+                     $('#payment-errors').show().text(message);
+                     event.preventDefault();
+                     $form.find('.submit').prop('disabled', false);
+                     return false;
+                     }*/
 
-            // Request a token from Stripe:
-            Stripe.card.createToken($form, stripeResponseHandler);
+                    // Request a token from Stripe:
+                    Stripe.card.createToken($form, stripeResponseHandler);
 
-            // Prevent the form from being submitted:
-              /*$form.find('.submit').prop('disabled', false);*/
-            $('#pay-now-button').prop('disabled', false);
-            event.preventDefault();
-            return false;
-          }
+                    // Prevent the form from being submitted:
+                    /*$form.find('.submit').prop('disabled', false);*/
+                    $('#pay-now-button').prop('disabled', false);
+                    event.preventDefault();
+                    return false;
+                }
+            });
         });
-      });
 
-      function stripeResponseHandler(status, response) {
-        // Grab the form:
-        var $form = $('#payment-form');
+        function stripeResponseHandler(status, response) {
+            // Grab the form:
+            var $form = $('#payment-form');
 
-        p('in response handler');
-        if (response.error) { // Problem!
+            p('in response handler');
+            if (response.error) { // Problem!
 
-          // Show the errors on the form:
-          $form.find('.payment-errors').text(response.error.message);
-          $form.find('.submit').prop('disabled', false); // Re-enable submission
+                // Show the errors on the form:
+                $form.find('.payment-errors').text(response.error.message);
+                $form.find('.submit').prop('disabled', false); // Re-enable submission
 
-        } else { // Token was created!
+            } else { // Token was created!
 
-          // Get the token ID:
-          var token = response.id;
+                // Get the token ID:
+                var token = response.id;
 
-          // Insert the token ID into the form so it gets submitted to the server:
-          $form.append($('<input type="hidden" name="stripeToken">').val(token));
+                // Insert the token ID into the form so it gets submitted to the server:
+                $form.append($('<input type="hidden" name="stripeToken">').val(token));
 
-          // Submit the form:
-          $form.get(0).submit();
+                // Submit the form:
+                $form.get(0).submit();
+            }
         }
-      }
 
     </script>
     <script src="{{ asset('js/checkout.js',env('APP_ENV') === 'production') }}"></script>
     <script>
-      $('#venmo-payment-div').hide();
-      $('#pay-with-venmo').on('change', function () {
-        var box = $(this);
-        if (box.is(':checked')) {
-          // set pay with venmo to true
-          $('#pay-with-venmo').val(1);
-          $('#venmo-payment-div').show(350);
-          $('#card-payment-div').hide(350);
-        } else {
-          // set pay with venmo to false
-          $('#pay-with-venmo').val(0);
-          $('#venmo-payment-div').hide(350);
-          $('#card-payment-div').show(350);
-        }
-      })
+        $('#venmo-payment-div').hide();
+        $('#pay-with-venmo').on('change', function () {
+            var box = $(this);
+            if (box.is(':checked')) {
+                // set pay with venmo to true
+                $('#pay-with-venmo').val(1);
+                $('#venmo-payment-div').show(350);
+                $('#card-payment-div').hide(350);
+            } else {
+                // set pay with venmo to false
+                $('#pay-with-venmo').val(0);
+                $('#venmo-payment-div').hide(350);
+                $('#card-payment-div').show(350);
+            }
+        })
     </script>
 @stop
