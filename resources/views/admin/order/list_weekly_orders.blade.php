88239cf47d719196ddd2eab01e4ef7f9e8687623
Daniel Bryce Evans <evansdb0@sewanee.edu>
2017-03-06 19:49:48 -0600

    Finished the order list

--- resources/views/admin/order/list_weekly_orders.blade.php
+++ resources/views/admin/order/list_weekly_orders.blade.php
@@ -6,16 +6,31 @@
 
 @section('body')
 
+    <ul>
+        <li>
+            Total Cost before fees: <span id="total-cost-before-fees"></span>
+        </li>
+        <li>
+            Profit <span id="profit"></span>
+        </li>
+        <li>
+            Tax Total: <span id="tax"></span>
+        </li>
+        <li>
+            Stripe Costs: <span id="stripe-cost"></span>
+        </li>
+    </ul>
+
     <ul class="list-group">
         @foreach($orders as $order)
 
-            <li class="list-group-item">
+            <li class="list-group-item order">
                 Customer Name: <h3>{{ $order->c_name }}</h3>
                 Customer Email: <h3>{{ $order->email_of_customer }}</h3>
                 <span class="order-price-before-fees">{{ $order->sumPriceBeforeFees() }}</span>
                 <ul>
                     @foreach($order->menuItemOrders as $menuItemOrder)
-                        <li>
+                        <li class="item">
                             Name: {{ $menuItemOrder->menuItem->name }}
                             <br>
                             Price: {{ $menuItemOrder->menuItem->price }}
@@ -36,12 +51,6 @@
         @endforeach
     </ul>
 
-    <ul>
-        <li>
-            Total Cost before fees: <span id="total-cost-before-fees"></span>
-        </li>
-    </ul>
-
     <script>
       $(function () {
         var totalBeforeFees = 0;
@@ -49,7 +58,29 @@
           p($(this).text());
           totalBeforeFees += parseFloat($(this).text())
         });
+        var profit = 0;
+        var numOrders = 0;
+        var numItems = 0;
+        var deliveryFee = 3;
+        var tax = 1.0925;
+        $('.order').each(function () {
+          numOrders++;
+        });
+        $('.item').each(function () {
+          numItems++;
+        });
+        profit = numItems + (numOrders * deliveryFee);
         $('#total-cost-before-fees').text(totalBeforeFees);
+        var totalForAllOrders = (profit + totalBeforeFees) * tax;
+
+        var totalForTax = totalForAllOrders * (1 - tax);
+        $('#tax').text(totalForTax);
+        var stripeCost = .3 + (.029 * totalForAllOrders);
+
+        $('#stripe-cost').text(stripeCost);
+
+        profit -= stripeCost;
+        $('#profit').text(profit);
       })
     </script>
 @stop
