eade7e6b38cf8634063c270e560b79c54861200c
blezzoh <iradub0@sewanee.edu>
2017-04-01 19:11:36 -0500

    weeklycost done, pricing done, cart subtotal done -- still need to do the actual cost

--- app/CustomClasses/ShoppingCart/WeeklyBilling.php
+++ app/CustomClasses/ShoppingCart/WeeklyBilling.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: blaise
+ * Date: 4/1/17
+ * Time: 3:57 PM
+ */
 
 namespace App\CustomClasses\ShoppingCart;
 
@@ -10,181 +16,41 @@ class WeeklyBilling
     protected $cart;
     protected $delivery_fee;
     protected $discount;
-    protected $subtotal;
-    protected $delivery_fee_reduction;
-    protected $total;
-    protected $tax;
-    protected $tax_percent;
-    protected $base_delivery_fee;
-    protected $cost_of_food;
-    protected $mark_up_per_item;
-    protected $stripe_fees;
-    protected $profit;
-
+    protected $number_of_items;
+    protected $items_with_discount;
+    protected $markup;
     /**
-     * CartBilling constructor.
      * @param ShoppingCart|null $cart For displaying business info, like prices, no need to pass a shopping cart
      * else pass the shopping cart to compute billing information
      */
     public function __construct(ShoppingCart $cart = null)
     {
         $this->cart = $cart;
-        $this->tax_percent = 1.0925;
-        $this->base_delivery_fee = 3;
-        $this->mark_up_per_item = .75;
-        $this->delivery_fee_reduction = .6;
-        // I KNOW YOU WON'T LISTEN BUT...
-        // THE ORDER OF THE FUNCTIONS BELOW MATTER, THEY BUILD ON EACH
-        // OTHER AND DO NOT PROVIDE ERROR CHECKING
-        $this->delivery_fee = $this->deliveryFee();
-        $this->discount = $this->discount();
-        $this->cost_of_food = $this->costOfFood();
-        $this->total = round($this->totalPrice(), 2);
-        $this->stripe_fees = $this->stripeFees();
-        $this->profit = $this->profit();
+        $this->delivery_fee = 3;
+        $this->discount = .6;
+        $this->number_of_items = $this->countWeeklyItems();
+        $this->items_with_discount = $this->countItemsWithDiscount();
+        $this->markup = 0.75;
     }
 
-    private function deliveryFee()
+    /**
+     * @return ShoppingCart|null
+     */
+    public function getCart()
     {
-        $num_items = $this->cart->getQuantity();
-        if ($num_items >= 4) {
-            return ($this->getBaseDeliveryFee() - ($this->getDeliveryFeeReduction() * 3));
-        }
-        /*
-         * If the $num_items = 0, then when we subtract 1 it will become negative
-         * The absolute value of that will be greater than the original $num_items
-         * (which is zero)
-         */
-        return $this->getBaseDeliveryFee() -
-            ($this->getDeliveryFeeReduction() * (min(abs($num_items - 1), $num_items)));
+        return $this->cart;
     }
 
     /**
      * @return int
-     */
-    public function getBaseDeliveryFee()
-    {
-        return $this->base_delivery_fee;
-    }
-
-
-    public function getDeliveryFeeReduction()
-    {
-        return $this->delivery_fee_reduction;
-    }
-
-    private function discount()
-    {
-        $num_items = $this->cart->getQuantity();
-        if ($num_items >= 3) {
-            return 60;
-        }
-        return ($num_items - 1) * 20;
-    }
-
-    public function costOfFood()
-    {
-        $cost_of_food = 0;
-        $cost_of_accessories = 0;
-        if (!empty($this->cart->items())) {
-            foreach ($this->cart->items() as $cart_item) {
-                $cost_of_food += $cart_item->getPrice();
-            }
-            foreach ($this->cart->items() as $cart_item) {
-                if (!empty($cart_item->getExtras())) {
-                    foreach ($cart_item->getExtras() as $extra_id) {
-                        $cost_of_accessories += Accessory::find($extra_id)->price;
-                    }
-                }
-            }
-        }
-        return $cost_of_food + $cost_of_accessories;
-    }
-
-    private function subtotal()
-    {
-        return $this->getDeliveryFee() + $this->getCostOfFood();
-    }
-
-    /**
-
-     * @return mixed
      */
     public function getDeliveryFee()
     {
         return $this->delivery_fee;
     }
 
-    public function getCostOfFood()
-    {
-        return $this->cost_of_food;
-    }
-
-    private function tax()
-    {
-        return 0;
-    }
-
-    private function totalPrice()
-    {
-        return $this->getSubtotal() * $this->getTaxPercent();
-    }
-
     /**
-     * @return mixed
-     */
-    public function getSubtotal()
-    {
-        return $this->subtotal;
-    }
-
-    /**
-     * @return float
-     */
-    public function getTaxPercent()
-    {
-        return $this->tax_percent;
-    }
-
-    public function stripeFees()
-    {
-        // stripe charges 30 cents + 2.9% -> .029
-        return .3 + $this->getTotal() * .029;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getTotal()
-    {
-        return $this->total;
-    }
-
-    public function profit()
-    {
-        // profit per order is the calculated delivery fee
-        // plus the mark up on each item * num items
-        // minus expenses i.e. stripe fees
-        return $this->getDeliveryFee() +
-            ($this->cart->getQuantity() * $this->getMarkUpPerItem())
-            - $this->getStripeFees();
-    }
-
-    /**
-     * @return float
-     */
-    public function getMarkUpPerItem()
-    {
-        return $this->mark_up_per_item;
-    }
-
-    public function getStripeFees()
-    {
-        return $this->stripe_fees;
-    }
-
-    /**
-     * @return mixed
+     * @return int
      */
     public function getDiscount()
     {
@@ -192,17 +58,45 @@ class WeeklyBilling
     }
 
     /**
-     * @return mixed
+     * @return array
      */
-    public function getTax()
+    public function getNumberOfItems()
     {
-        return $this->tax;
+        return $this->number_of_items;
     }
 
-    public function getProfit()
+    /**
+     * @return array|int
+     */
+    public function getItemsWithDiscount()
     {
-        return $this->profit;
+        return $this->items_with_discount;
     }
 
 
+
+    public function countItemsWithDiscount()
+    {
+        if($this->number_of_items < 5 && $this->number_of_items >1){
+            return $this->number_of_items -1;
+        }
+        else if($this->number_of_items >= 5){
+            return 3;
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public function countWeeklyItems()
+    {
+        return($this->cart->getWeeklySpecialItems());
+    }
+    /**
+     * @return float
+     */
+    public function getMarkup()
+    {
+        return $this->markup;
+    }
 }
