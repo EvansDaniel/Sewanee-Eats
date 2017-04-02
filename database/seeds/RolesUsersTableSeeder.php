d9f3e6e2da8358c679759a06b967beabbf5836df
Daniel <evansdb0@sewanee.edu>
2017-04-02 15:05:16 -0500

    Added todo for Blaise

--- database/seeds/RolesUsersTableSeeder.php
+++ database/seeds/RolesUsersTableSeeder.php
@@ -26,35 +26,37 @@ class RolesUsersTableSeeder extends Seeder
         $blaise->roles()->attach([$admin_role->id, $manager_role->id]);
         $tari->roles()->attach([$admin_role->id, $manager_role->id]);
 
-        $num_couriers = 10;
+        if (env('APP_ENV') == "local") {
+            $num_couriers = 10;
 
-        $faker = Faker\Factory::create();
-        for ($i = 0; $i < $num_couriers; $i++) {
+            $faker = Faker\Factory::create();
+            for ($i = 0; $i < $num_couriers; $i++) {
+                $courier = new User;
+                $courier->name = $faker->name;
+                $courier->email = $faker->companyEmail;
+                $courier->password = bcrypt("mypass");
+                $courier->remember_token = str_random(10);
+                $courier->save();
+                $courier->roles()->attach($courier_role->id);
+                $courier_info = new CourierInfo;
+                $courier_info->phone_number = "9316913594";
+                $courier_info->is_delivering_order = false;
+                $courier_info->user_id = $courier->id;
+                $courier_info->save();
+            }
             $courier = new User;
-            $courier->name = $faker->name;
-            $courier->email = $faker->companyEmail;
-            $courier->password = bcrypt("mypass");
+            $courier->name = 'Test Courier';
+            $courier->email = 't@t.com';
+            $courier->password = bcrypt('dsmith');
             $courier->remember_token = str_random(10);
             $courier->save();
             $courier->roles()->attach($courier_role->id);
             $courier_info = new CourierInfo;
             $courier_info->phone_number = "9316913594";
             $courier_info->is_delivering_order = false;
+            $courier_info->current_order_id = 0;
             $courier_info->user_id = $courier->id;
             $courier_info->save();
         }
-        $courier = new User;
-        $courier->name = 'Test Courier';
-        $courier->email = 't@t.com';
-        $courier->password = bcrypt('dsmith');
-        $courier->remember_token = str_random(10);
-        $courier->save();
-        $courier->roles()->attach($courier_role->id);
-        $courier_info = new CourierInfo;
-        $courier_info->phone_number = "9316913594";
-        $courier_info->is_delivering_order = false;
-        $courier_info->current_order_id = 0;
-        $courier_info->user_id = $courier->id;
-        $courier_info->save();
     }
 }
