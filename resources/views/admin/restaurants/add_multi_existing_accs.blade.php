9eee60810b8748b7edea29cce6ea63f95cbc1760
Daniel Bryce Evans <evansdb0@sewanee.edu>
2017-04-07 03:35:05 -0500

    Can add existing accessories to menu items now

--- resources/views/admin/restaurants/add_multi_existing_accs.blade.php
+++ resources/views/admin/restaurants/add_multi_existing_accs.blade.php
@@ -8,20 +8,25 @@
     <div class="container" id="new-open-time-container">
         <p>Add Multiple Accessories to {{ $menu_item->name }}</p>
         <div>
-            <form action="{{ url()->to(parse_url(route('createMultiAddAccs',[]),PHP_URL_PATH),[],env('APP_ENV') !== 'local') }}"
-                  method="post">
-                {{ csrf_field() }}
-                <input name="menu_item_id" type="hidden" value="{{ $menu_item->id }}">
-                <div class="form-group">
-                    <select style="height: 150px" name="accessories[]" id="" class="form-control" multiple>
-                        @foreach($accs as $acc)
-                            <option value="{{ $acc->id }}">{{ $acc->name }} | ${{ $acc->price }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <br>
-                <button class="btn btn-primary">Add All Selected Accessories to {{ $menu_item->name }}</button>
-            </form>
+            @if(empty($accs))
+                <h2>There seems to be no existing accessories that aren't already attached to this menu item. <a
+                            href="{{ route('adminShowMenu') }}">Back to Menu</a></h2>
+            @else
+                <form action="{{ url()->to(parse_url(route('createMultiAddAccs',[]),PHP_URL_PATH),[],env('APP_ENV') !== 'local') }}"
+                      method="post">
+                    {{ csrf_field() }}
+                    <input name="menu_item_id" type="hidden" value="{{ $menu_item->id }}">
+                    <div class="form-group">
+                        <select style="height: 150px" name="accessories[]" id="" class="form-control" multiple>
+                            @foreach($accs as $acc)
+                                <option value="{{ $acc->id }}">{{ $acc->name }} | ${{ $acc->price }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <br>
+                    <button class="btn btn-primary">Add All Selected Accessories to {{ $menu_item->name }}</button>
+                </form>
+            @endif
         </div>
     </div>
 @stop
