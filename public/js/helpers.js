4868697a2f9562e62b800a2f4f1815bfe80b4bd7
Daniel <evansdb0@sewanee.edu>
2017-04-13 06:49:39 -0500

    More visual improvements

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,4 +1,5 @@
 function setWindowConfirmation(buttonIdentifier, confirmationText) {
+  // if the user declines, don't perform the default action
   $(buttonIdentifier).on('click', function () {
     if (!window.confirm(confirmationText)) {
       return false;
